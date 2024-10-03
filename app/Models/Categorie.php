c86a3d2cc0790518a04c93baea543ad0018eb8f9
taha-daoued <daoudt111@gmail.com>
2024-10-03 11:57:33 +0100

    first commit

---
+++ app/Models/Categorie.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nomcategorie','imgecategorie'
+    ];
+    public function scategories()
+    {
+    return $this->hasMany(Scategorie::class ,"categorieID");
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ScategorieController;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::middleware('api')->group(function () {
+    Route::resource('categories', CategorieController::class);
+    });
+    Route::get("/categories",[CategorieController::class,"index"]);
+    Route::post("/categories",[CategorieController::class,"store"]);
+    Route::get("/categories/{id}",[CategorieController::class,"show"]);
+    Route::delete("/categories/{id}",[CategorieController::class,"destroy"]);
+    Route::put("/categories/{id}",[CategorieController::class,"update"]);
+    Route::middleware('api')->group(function () {
+
+    Route::resource('scategories', ScategorieController::class);
+
+    });
+    Route::get('/scat/{idcat}', [ScategorieController::class,'showSCategorieByCAT']);
+    Route::middleware('api')->group(function () {
+
+    Route::resource('scategories', ScategorieController::class);
+
+    });
+    Route::get('/lisarticles/{idcat}', [ScategorieController::class,'showSCategorieByCAT']);
+    Route::middleware('api')->group(function () {
+        Route::resource('articles', ArticleController::class);
+        });
+        Route::get('/articles/art/articlespaginate', [ArticleController::class,
+        'articlesPaginate']);
